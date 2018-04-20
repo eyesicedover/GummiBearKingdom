ef078f75f3819fb32ffaf27af9aa5974ee6ce611
Stephanie Faber <eyesicedover@gmail.com>
2018-04-20 09:13:38 -0700

    add entity framework core, add Product model

---
+++ GummiBearKingdom/Models/GBKContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GummiBearKingdom.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        //public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;Port=8889;database=gbk;uid=root;pwd=root;");
+    }
+}
--- GummiBearKingdom/Startup.cs
+++ GummiBearKingdom/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -19,13 +20,17 @@ namespace GummiBearKingdom
         {
             var builder = new ConfigurationBuilder()
               .SetBasePath(env.ContentRootPath)
-              .AddEnvironmentVariables();
+              .AddJsonFile("appsettings.json");
             Configuration = builder.Build();
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddEntityFrameworkMySql()
+           .AddDbContext<GBKContext>(options =>
+                                     options
+                                          .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
