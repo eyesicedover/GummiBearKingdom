50073978a8c4957401341ce4dd453e7e67a0ba99
Stephanie Faber <eyesicedover@gmail.com>
2018-04-20 15:12:14 -0700

    add edit and delete functionality

--- GummiBearKingdom/Controllers/ProductsController.cs
+++ GummiBearKingdom/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using GummiBearKingdom.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -33,6 +34,35 @@ namespace GummiBearKingdom.Controllers
         public IActionResult Create(Product product)
         {
             db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(p => p.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(p => p.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(p => p.ProductId == id);
+            db.Products.Remove(thisProduct);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
