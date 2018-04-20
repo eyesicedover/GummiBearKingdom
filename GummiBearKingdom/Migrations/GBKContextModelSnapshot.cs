1fb479f6666e5ac466991f6157fa98b3b08190bb
Stephanie Faber <eyesicedover@gmail.com>
2018-04-20 10:23:08 -0700

    add db migration

---
+++ GummiBearKingdom/Migrations/GBKContextModelSnapshot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Migrations
+{
+    [DbContext(typeof(GBKContext))]
+    partial class GBKContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("GummiBearKingdom.Models.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cost");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ProductId");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
