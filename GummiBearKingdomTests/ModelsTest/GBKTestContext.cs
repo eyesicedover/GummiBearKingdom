624972882fb1cbc985713a29d830eb931da31046
Stephanie Faber <eyesicedover@gmail.com>
2018-05-02 14:54:15 -0700

    add test for GetAverageRating

---
+++ GummiBearKingdomTests/ModelsTest/GBKTestContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GummiBearKingdom.Models
+{
+    public class GBKTestContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"Server=localhost;Port=8889;database=gbktest;uid=root;pwd=root;");
+        }         
+    }
+}
---
+++ GummiBearKingdomTests/ModelsTest/ProductTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GummiBearKingdom.Models;
+
+namespace Tests.ModelsTest
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void Product_Constructor_Test()
+        {
+            //Arrange
+            string description = "lil bits of black licorice";
+
+            //Act
+            Product newProduct = new Product("Black Licorice", description, 2.44);
+            newProduct.ProductId = 1;
+            newProduct.Reviews = null;
+
+            //Assert
+            Assert.AreEqual(description, newProduct.Description);
+        }
+
+        [TestMethod]
+        public void GetAverageRating_Test()
+        {
+            //Arrange
+            Product newProduct = new Product("Black Licorice", "lil bits of black licorice", 2.44);
+            newProduct.ProductId = 1;
+            List<Review> theReviews = new List<Review>
+            {
+                new Review("Jerry", "Love dat flavor", 5),
+                new Review("Sam", "Made me yartz", 1),
+                new Review("Claire", "Pretty good", 3)
+            };
+            newProduct.Reviews = theReviews;
+
+            //Act
+            double result = newProduct.GetAverageRating();
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
